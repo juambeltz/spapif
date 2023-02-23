bd30447b33b027e39a976bc1c16c7ca44b5011bf
Juan Ignacio Juambeltz <112590634+juambeltz@users.noreply.github.com>
2023-02-23 11:33:59 -0300

    modifique el title del index y cree carpeta js co archivo menu.js adentro

---
+++ js/menu.js
@@ -0,0 +1,5 @@
+const hamburger = document.getElementById('hamburger');
+const navCelesteOpciones = document.getElementById('nav-celeste-opciones');
+hamburger.addEventListener('click', () => {
+navCelesteOpciones.classList.toggle('mostrar');}
+);
